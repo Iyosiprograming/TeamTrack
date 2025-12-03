af102d464113ac8a458b0cfee611e42599273026
Iyosiyas <Iyosieyosiyas@gmail.com>
2025-12-03 02:18:33 -1000

    setup the owner login

--- backend/src/controllers/ownerController.ts
+++ backend/src/controllers/ownerController.ts
@@ -1,7 +1,8 @@
 import type { Request, Response } from "express";
 import ownerModel from "../models/ownerModel.js";
 import bcrypt from "bcrypt";
-
+import jwt from "jsonwebtoken"
+const JWT_SECRET = process.env.JWT_SECRET || "Hello world"
 // create owner
 
 export const createOwner = async (req: Request, res: Response) => {
@@ -26,3 +27,36 @@ export const createOwner = async (req: Request, res: Response) => {
         return res.status(500).json({ message: "Internal server error" })
     }
 };
+
+// login owner
+export const loginOwner = async (req: Request, res: Response) => {
+    const { email, password, passcode } = req.body
+    const existingOwner = await ownerModel.findOne({ email })
+    if (!existingOwner) {
+        return res.status(400).json({
+            message: "Ownner not Found",
+        })
+    }
+    if (passcode === existingOwner.passcode) {
+        const isPasswordmatch = await bcrypt.compare(password, existingOwner.password)
+
+        if (isPasswordmatch) {
+            const token = jwt.sign(
+                {
+                    ownerId: existingOwner._id,
+                    email: existingOwner.email
+                },
+                JWT_SECRET,
+                {
+                    expiresIn: '1h',
+                }
+            );
+            res.status(200).json({
+                messaeg: `Welcome ${existingOwner.name}👋`,
+                token
+            })
+        }
+    }
+
+
+}
--- backend/src/routes/ownerRouter.ts
+++ backend/src/routes/ownerRouter.ts
@@ -1,8 +1,10 @@
 import express from "express";
-import { createOwner } from "../controllers/ownerController.js";
+import { createOwner,loginOwner } from "../controllers/ownerController.js";
 
 const router = express.Router();
 
 router.post("/create", createOwner)
+
+router.post("/login", loginOwner)
 
 export default router
