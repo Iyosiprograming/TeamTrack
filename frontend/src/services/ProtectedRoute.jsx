d0f4a81c608f500cf76ef728c979c97ee7f25acb
Iyosiyas <Iyosieyosiyas@gmail.com>
2025-12-12 06:22:10 -1000

    removed all the react because of it's not ts

--- frontend/src/services/ProtectedRoute.jsx
+++
@@ -1,7 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-export const ProtectedRoute = ({ children }) => {
-    const isAuth = document.cookie.includes("jwt=");
-
-    return isAuth ? children : <Navigate to="/login" />;
-};
